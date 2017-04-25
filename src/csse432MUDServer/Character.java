dc627df4ea0c139ef82bcb5fd7ce879f9727886e
Tim Smith <timothys740@gmail.com>
2017-04-24 23:41:18 -0400

    Starting chracter/player framework

---
+++ src/csse432MUDServer/Character.java
@@ -0,0 +1,19 @@
+package csse432MUDServer;
+
+import java.util.ArrayList;
+
+public abstract class Character {
+	ArrayList<String> Inventory = new ArrayList<String>();
+	
+	ArrayList<String> getInventory(){
+		return Inventory;
+	}
+	
+	void addToInventory(String item){
+		Inventory.add(item);
+	}
+	
+	void removeFromInventory(String item){
+		Inventory.remove(item);
+	}
+}
