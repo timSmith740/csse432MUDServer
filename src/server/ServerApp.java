20dc7194d285ed2e69aacbeb5d476b160a61f8c2
Logan Erexson <erexsolr@rose-hulman.edu>
2017-05-19 11:22:39 -0400

    Added argument functionality to serverApp

--- src/server/ServerApp.java
+++ src/server/ServerApp.java
@@ -10,11 +10,15 @@ public class ServerApp {
 
 	public static void main(String[] args) {
 		System.out.println("Server Running");
-		
-		Server myServer = new Server(ServerProtocol.DEFAULT_PORT, ServerProtocol.CHAT_PORT);
+		int port =ServerProtocol.DEFAULT_PORT;
+		int chatPort=ServerProtocol.CHAT_PORT;
+		if(args.length==2){
+			port= Integer.parseInt(args[0]);
+			chatPort=Integer.parseInt(args[1]);
+		}
+		Server myServer = new Server(port, chatPort);
 		
 		myServer.execute();
-			
 		
 	}
 
